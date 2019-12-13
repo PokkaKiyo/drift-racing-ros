a774d7e23fae31f9e67f89cd6546d6aca48d8039
PokkaKiyo <e0030925@u.nus.edu>
2019-12-13 21:00:05 +0800

    add comments to describe joystick teleop node

--- src/drift_racer/src/joystick_teleop.cpp
+++ src/drift_racer/src/joystick_teleop.cpp
@@ -1,3 +1,19 @@
+/*
+ * This node has 2 responsibilities:
+ * 1. Interpret the sensor_msgs/Joy messages from the /joy topic
+ * 2. Arbitrate between sending manual and autonomous commands to the actuators
+ * 
+ * Output: A message of type geometry_msgs/Twist, where
+ * msg.linear.x is the desired speed of the vehicle
+ * msg.linear.z is 1 if the brakes are activated, 0 otherwise
+ * msg.angular.z is the desired steering angle of the vehicle
+ * All other fields should be 0
+ * 
+ * Callbacks:
+ * 1. joystickCallback: interprets the Joy messages and publishes manual and brake commands
+ * 2. navCmdVelCallback: forwards the cmd_vel_nav message iff the vehicle is in autonomous mode
+ */
+
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/Joy.h>
@@ -30,8 +46,8 @@ private:
   double MAX_SPEED = 1.0;
   double MAX_STEERING_ANGLE = 0.8;
 
-  void joystickCallback(const sensor_msgs::Joy::ConstPtr& joy_msg);
-  void navCmdVelCallback(const geometry_msgs::Twist::ConstPtr& nav_cmd_vel_msg);
+  void joystickCallback(const sensor_msgs::Joy::ConstPtr &joy_msg);
+  void navCmdVelCallback(const geometry_msgs::Twist::ConstPtr &nav_cmd_vel_msg);
 };
 
 JoystickTeleop::JoystickTeleop()
@@ -53,7 +69,7 @@ JoystickTeleop::JoystickTeleop()
   current_nav_mode_ = NavMode::Brake;
 }
 
-void JoystickTeleop::joystickCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
+void JoystickTeleop::joystickCallback(const sensor_msgs::Joy::ConstPtr &joy_msg)
 {
   // Step 1: map the joy_msg fields to the physical buttons
   int buttonA = joy_msg->buttons[0];
@@ -105,7 +121,7 @@ void JoystickTeleop::joystickCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
     {
       cmd_vel_manual_.linear.x = (forward_axes - 0.1) * MAX_SPEED;
     }
-    
+
     cmd_vel_manual_.angular.z = steering_axes * MAX_STEERING_ANGLE;
     cmd_vel_manual_.linear.z = 0;
     cmd_vel_pub_.publish(cmd_vel_manual_);
@@ -123,7 +139,7 @@ void JoystickTeleop::joystickCallback(const sensor_msgs::Joy::ConstPtr& joy_msg)
   }
 }
 
-void JoystickTeleop::navCmdVelCallback(const geometry_msgs::Twist::ConstPtr& nav_cmd_vel_msg)
+void JoystickTeleop::navCmdVelCallback(const geometry_msgs::Twist::ConstPtr &nav_cmd_vel_msg)
 {
   if (current_nav_mode_ == NavMode::Autonomous)
   {
@@ -138,7 +154,7 @@ void JoystickTeleop::navCmdVelCallback(const geometry_msgs::Twist::ConstPtr& nav
   }
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   ros::init(argc, argv, "joystick_teleop_node");
   JoystickTeleop joystick_teleop_obj;
