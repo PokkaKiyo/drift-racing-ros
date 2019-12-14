981913251ab6b05e4488f4075234d00d89e8c14c
PokkaKiyo <e0030925@u.nus.edu>
2019-12-14 14:05:52 +0800

    Update actuators_controller.py

--- src/drift_racer/src/actuators_controller.py
+++ src/drift_racer/src/actuators_controller.py
@@ -17,24 +17,45 @@ Callbacks:
 import rospy
 from geometry_msgs.msg import Twist
 
+import RPi.GPIO as IO
+from gpiozero import AngularServo
+
+
 class ActuatorsController:
     def __init__(self):
         self._cmd_vel_sub = rospy.Subscriber("/cmd_vel_final", Twist, self.cmd_vel_callback)
-    
+
+        self._max_speed = 2.0
+
+        IO.setwarnings(False)
+        IO.setmode(IO.BCM)
+        IO.setup(12, IO.OUT)
+        IO.setup(23, IO.OUT, initial=1)
+        self._p = IO.PWM(12, 500)
+        self._p.start(0)
+
+        self._servo = AngularServo(13, min_angle=-90, max_angle=90)
+
+
     def cmd_vel_callback(self, msg):
+        # Step 1: Control the speed
         if msg.linear.z != 0:
-            # TODO: write brake code here (set speed to 0)
-            print("Braking!") # stub
+            desired_speed = 0
+            print("Actuators: Braking!")
         else:
             desired_speed = msg.linear.x
-            # TODO: write speed control code here
-            print("The desired speed is", desired_speed)
-        
+
+        print("Actuators: The desired speed is", desired_speed)
+        self._p.ChangeDutyCycle(desired_speed / self._max_speed * 50 + 50)
+
+        # Step 2: Control the steering
         desired_steering_angle = msg.angular.z
-        # TODO: write steering controller code here
-        print("The desired steering angle is", desired_steering_angle)
+        self._servo.angle = desired_steering_angle * 180
+        print("Actuators: The desired steering angle is", desired_steering_angle)
     
+
 if __name__ == '__main__':
     rospy.init_node('actuators_controller')
+    rospy.logwarn("Actuators Controller: This code is untested, do not put on the car!")
     ActuatorsController()
     rospy.spin()
