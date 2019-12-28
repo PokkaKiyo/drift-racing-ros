f63e3fe84f8ebf951474a94d1ba170ee9b5fd9d5
PokkaKiyo <31039465+PokkaKiyo@users.noreply.github.com>
2019-12-28 17:16:08 +0800

    Upload Code for Drive by Wire 1.0 (#7)
    * Add files via upload
    
    * Delete camera_capture.py
    
    * Delete camera_image_retriever.py
    
    * Add files via upload

--- src/drift_racer/src/camera_capture.py
+++ src/drift_racer/src/camera_capture.py
@@ -1,38 +1,35 @@
 #!/usr/bin/env python
 
-'''
-
-This node is responsible for capturing images using the camera on the vehicle.
-
-https://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
-
-https://picamera.readthedocs.io/en/release-1.10/recipes1.html
-
-Note: if the camera and neural network stuff do not run well with ROS, we can run them seperately
-and then use a local socket to transport the steering angle to a publisher node
-
-'''
-
 import rospy
-from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 import picamera
+import io
+from PIL import Image
 
 class CameraCapture:
     def __init__(self):
-        self._image_pub = rospy.Publisher('/cam_image', Image, queue_size=1)
-        self._camera  = picamera.camera.PiCamera()
+        self._image_count_pub = rospy.Publisher('/image_count', String, queue_size=1)
+        self._image_count = 0
+        rospy.loginfo("CameraCapture: starting camera stream.")
         self.start_camera()
     
     def start_camera(self):
-        # for testing
-        self._camera.capture('example.jpg')
+        with picamera.PiCamera(resolution=(640, 480), framerate=30) as camera:
+            try:
+                stream = io.BytesIO()
+                for _ in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
+                    stream.seek(0)
+                    image = Image.open(stream).convert('RGB')
+                    img_save_path = "/media/pi/tamano/data/" + str(self._image_count) + ".jpg"
+                    image.save(img_save_path)
 
-        # TODO: send the message over the /cam_image topic
-
-        # TODO: put in a while loop
-
-
+                    self._image_count_pub.publish(str(self._image_count))
+                    self._image_count += 1
+            except KeyboardInterrupt:
+                rospy.loginfo("keyboard interrupt")
+            finally:
+                rospy.loginfo("done")
 
 if __name__ == '__main__':
     rospy.init_node('camera_capture')
---
+++ src/drift_racer/src/training_data_collector.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+import picamera
+import io
+import csv
+from PIL import Image
+
+class TrainingDataCollector:
+    def __init__(self):
+        self._cmd_vel_sub = rospy.Subscriber("/cmd_vel_final", Twist, self.cmd_vel_callback)
+        self._image_count_sub = rospy.Subscriber("/image_count", String, self.image_count_callback)
+
+        self._steering_angle = 0
+        self._speed = 0
+
+        with open('data_log.csv', 'w+') as file:
+            writer = csv.writer(file)
+            writer.writerow(['image_path', 'steering_angle', 'speed'])
+
+        rospy.loginfo("TrainingDataCollector: started")
+
+    def cmd_vel_callback(self, msg):
+        self._steering_angle = msg.angular.z
+        self._speed = msg.linear.x
+    
+    def image_count_callback(self, msg):
+        image_path = "data/" + msg.data + ".jpeg"
+
+        with open('data_log.csv', 'a+') as file:
+            writer = csv.writer(file)
+            writer.writerow([image_path, self._steering_angle, self._speed])
+
+
+
+if __name__ == "__main__":
+    rospy.init_node('training_data_collector')
+    TrainingDataCollector()
+    rospy.spin()
